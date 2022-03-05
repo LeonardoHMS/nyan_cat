80edc146381010ab34fe3cfb9395d77bd8e40117
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-03-05 14:17:29 -0300

    melhorias
    Melhorado o visual do Nyan, ainda fixo

--- manage.py
+++ manage.py
@@ -71,5 +71,5 @@ while True:
         screen.blit(line_blue_skin, pos)
     for pos in line_violet:
         screen.blit(line_violet_skin, pos)
-    screen.blit(nyan_cat, (-380,5))
+    screen.blit(nyan_cat, (80, 190))
     pygame.display.update()
