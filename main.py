bc11d84541b5d11b5d9d9bc834a0555d59305676
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-04-01 20:10:01 -0300

    Rainbow
    Foi criada uma função rainbow para formação do arco-íris, que resulta na remoção de várias linhas que o formavam

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Nyan Cat')
 clock = pygame.time.Clock()
 
+def rainbow(pos, color):
+    line = [(0, pos[0]), (30, pos[1]),(60, pos[1]), (90, pos[0]), (120, pos[0]), (150, pos[1])]
+    line_skin = pygame.Surface((30, 15))
+    line_skin.fill(color)
+    for pos in line:
+        screen.blit(line_skin, pos)
+
 #Nyan Sprite
 class Nyan(pygame.sprite.Sprite):
     def __init__(self):
@@ -62,7 +69,6 @@ bomb_pix_skin = pygame.Surface((10,10))
 bomb_pix_skin.fill((255,250,250))
 
 # Lines Color
-surface = (30, 15)
 pos_red = pos_orange = pos_yellow = pos_green = pos_blue = pos_violet = loop = 0
 while True:
     if loop % 2 == 0:
@@ -80,30 +86,6 @@ while True:
         pos_blue = (305, 300)
         pos_violet = (320, 315)
     loop += 1
-
-    line_red = [(0, pos_red[0]), (30, pos_red[1]),(60, pos_red[1]), (90, pos_red[0]), (120, pos_red[0]), (150, pos_red[1])]
-    line_red_skin = pygame.Surface(surface)
-    line_red_skin.fill((255,0,0))
-
-    line_orange = [(0, pos_orange[0]), (30, pos_orange[1]),(60, pos_orange[1]), (90, pos_orange[0]), (120, pos_orange[0]), (150, pos_orange[1])]
-    line_orange_skin = pygame.Surface(surface)
-    line_orange_skin.fill((255,140,0))
-
-    line_yellow = [(0, pos_yellow[0]), (30, pos_yellow[1]),(60, pos_yellow[1]), (90, pos_yellow[0]), (120, pos_yellow[0]), (150, pos_yellow[1])]
-    line_yellow_skin = pygame.Surface(surface)
-    line_yellow_skin.fill((255,255,0))
-
-    line_green = [(0, pos_green[0]), (30, pos_green[1]),(60, pos_green[1]), (90, pos_green[0]), (120, pos_green[0]), (150, pos_green[1])]
-    line_green_skin = pygame.Surface(surface)
-    line_green_skin.fill((0,255,0))
-
-    line_blue = [(0, pos_blue[0]), (30, pos_blue[1]),(60, pos_blue[1]), (90, pos_blue[0]), (120, pos_blue[0]), (150, pos_blue[1])]
-    line_blue_skin = pygame.Surface(surface)
-    line_blue_skin.fill((0,0,255))
-
-    line_violet = [(0, pos_violet[0]), (30, pos_violet[1]),(60, pos_violet[1]), (90, pos_violet[0]), (120, pos_violet[0]), (150, pos_violet[1])]
-    line_violet_skin = pygame.Surface(surface)
-    line_violet_skin.fill((255,0,255))
 
 # Move Rainbow and Nyan Cat and Bomb Spawn
     clock.tick(7)
@@ -136,18 +118,12 @@ while True:
     except:
         pass
 # Rainbow
-    for pos in line_red:
-        screen.blit(line_red_skin, pos)
-    for pos in line_orange:
-        screen.blit(line_orange_skin, pos)
-    for pos in line_yellow:
-        screen.blit(line_yellow_skin, pos)
-    for pos in line_green:
-        screen.blit(line_green_skin, pos)
-    for pos in line_blue:
-        screen.blit(line_blue_skin, pos)
-    for pos in line_violet:
-        screen.blit(line_violet_skin, pos)
+    rainbow(pos_red, (255,0,0))
+    rainbow(pos_orange, (255,140,0))
+    rainbow(pos_yellow, (255,255,0))
+    rainbow(pos_green, (0,255,0))
+    rainbow(pos_blue, (0,0,255))
+    rainbow(pos_violet, (255,0,255))
 # Nyan
     sprites_nyan.draw(screen)
     sprites_nyan.update()
