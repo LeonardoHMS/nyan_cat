c9e450dd29e36d2ae1099442a737f2f1e8f48261
Leonardo Henrique Mantovani Silva <LeonardoHMS_Git@hotmail.com>
2022-05-15 15:47:30 -0300

    Upgrade

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\cursoemvideo\nyan_cat\venv
+set VIRTUAL_ENV=C:\Users\Leonardo Mantovani\Documents\Github\nyan_cat\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
